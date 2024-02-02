3c3ee5e50c6130a1e0d22101a4e03a94513a57bb
Ulugbek-Muqimjonov <muqimjonovulugbekgidgub@gmail.com>
2024-02-02 23:35:53 +0500

    add nabvar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from './components/navbar/NAvbar'
+import Body from './components/body/Body'
+import './App.css'
 
 function App() {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar/>
+      <Body/>
+    </div>
   )
 }
 
